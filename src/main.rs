57bc3a27fefb1359126a3b118e37a2576069f54f
abdullah <44318011+fx2y@users.noreply.github.com>
2019-12-22 09:32:02 +0700

    REPL
    Signed-off-by: abdullah <44318011+fx2y@users.noreply.github.com>

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate rustyline;
+
+use rustyline::Editor;
+use rustyline::error::ReadlineError;
+
+fn main() {
+    let mut rl = Editor::<()>::new();
+    if rl.load_history(".mal-history").is_err() {
+        eprintln!("No previous history.");
+    }
+
+    loop {
+        let readline = rl.readline("user> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+                rl.save_history(".mal-history").unwrap();
+                if line.len() > 0 {
+                    println!("{}", line);
+                }
+            }
+            Err(ReadlineError::Interrupted) => continue,
+            Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+}
